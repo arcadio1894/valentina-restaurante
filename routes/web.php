f39089bacc7c30639ea10f7a938b1afa1364246a
Jorge Gonzales Castillo <joryes1894@gmail.com>
2020-02-28 11:22:17 -0500

    Fix error login

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
+	Route::get('/home', 'HomeController@index')->name('home');
+
 	Route::prefix('admins')->group(function() {
 		Route::name('admins.')->group(function() {
     		Route::get('/', function () {
