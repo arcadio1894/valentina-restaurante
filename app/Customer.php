3046c7a2fe346fb4c3229c8c07b12d3b353eaf0f
SoleSCaverO <edilberto0905@gmail.com>
2020-04-14 22:04:05 -0500

    fix(Customer): Agregar líneas de código borradas en merge

--- app/Customer.php
+++ app/Customer.php
@@ -2,13 +2,18 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
+    use Notifiable;
     use SoftDeletes;
-    
+
+    protected $guard = 'customer';
+    protected $primaryKey = 'id';
+
     protected $fillable = [
         'name',
         'email',
@@ -18,6 +23,11 @@ class Customer extends Model
         'birthday',
         'genre',
         'phone',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token'
     ];
 
     public function user() {
